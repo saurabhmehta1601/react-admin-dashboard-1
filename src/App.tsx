2a5fd10ff4977a83aa5cd7ebf70639a203077108
saurabh <saurabhmehta1601@gmail.com>
2023-06-08 15:09:44 +0530

    feat: setup react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { ThemeProvider } from "@emotion/react"
 import { ColorModeContext, useMode } from "./theme"
 import { CssBaseline } from "@mui/material"
 import Topbar from "./scenes/global/Topbar"
+import { Route, Routes } from "react-router-dom"
+import Dashboard from "./scenes/dashboard"
 
 function App() {
   const [theme, colorMode] = useMode()
@@ -11,8 +13,12 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content"></main>
-          <Topbar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
