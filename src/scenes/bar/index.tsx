764329a43d91fb8c24db7c9522826b132ceb7a7f
saurabh <saurabhmehta1601@gmail.com>
2023-06-11 12:24:30 +0530

    add bar chart

--- src/scenes/bar/index.tsx
+++ src/scenes/bar/index.tsx
@@ -1,9 +1,15 @@
+import BarChart from '../../components/BarChart'
 import Header from '../../components/Header'
-
+import Box from '@mui/material/Box'
+  
 const Bar = () => {
+  
   return (
     <div>
       <Header heading="Bar Chart" subHeading='Simple Bar Chart' />
+      <Box sx={{ height: "500px", width: "100%" }}>
+        <BarChart />
+      </Box>
     </div>
   )
 }
