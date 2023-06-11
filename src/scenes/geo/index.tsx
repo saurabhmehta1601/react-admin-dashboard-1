7b77d9f7a952b7062fbfd32d6b4616a73100490c
saurabh <saurabhmehta1601@gmail.com>
2023-06-11 17:01:48 +0530

    add border to geoMap

--- src/scenes/geo/index.tsx
+++ src/scenes/geo/index.tsx
@@ -1,12 +1,20 @@
+import { useTheme } from '@mui/material'
 import ChoroplethChart from '../../components/ChoroplethChart'
 import Header from '../../components/Header'
 import Box from "@mui/material/Box"
+import { tokens } from '../../theme'
 
 const Geo = () => {
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+
   return (
     <Box>
       <Header heading='GEO' subHeading='Simple Geography Chart' />
-      <Box width="100%" height={"500px"} >
+      <Box width="100%" height={"500px"}
+        border={`1px solid ${colors.grey[100]}`}
+        borderRadius={"4px"}
+      >
         <ChoroplethChart />
       </Box>
     </Box>
